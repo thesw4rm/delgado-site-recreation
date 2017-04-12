9f31fb0f7d3ea6b64273568d9ecd535fafa4c26d
Yadunandan Pillai <ytpillai@gmail.com>
2017-04-12 00:07:26 -0400

    created basic design of intro

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+    '/',
+    function (req, res, next) {
+        var result_options = {
+            title: "eLearning Homepage string",
+            username: req.query.username,
+        };
+
+
+        res.render(
+            'index',
+            result_options
+        );
+    }
+);
 
 module.exports = router;
