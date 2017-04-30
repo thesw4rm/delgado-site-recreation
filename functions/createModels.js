1624ee7ca943ca7d1d2e9acecb67a897b9d2fe32
Yadunandan Pillai <ytpillai@gmail.com>
2017-04-30 13:08:29 -0400

    initial commit

---
+++ functions/createModels.js
@@ -0,0 +1,25 @@
+function createCoachesModel() {
+    var CoachesSchema = mongoose.Schema(
+        {
+            _id: Number,
+            nsme: String,
+            role: String,
+            img: String,
+            about: String,
+            quote: String,
+            quote_author: String,
+            gmail: String,
+            youtube: String,
+            facebook: String,
+            instagram: String,
+            hangouts: String,
+            ycbme: String
+        }
+    );
+    var Coaches = mongoose.model("Coaches", CoachesSchema);
+    return Coaches;
+}
+
+module.exports = {
+    coachesModel: createCoachesModel()
+}
---
+++ routes/api.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get(
+    '/coaches',
+    function (req, res, next) {
+        var Coaches = models.coachesModel;
+        Coaches.find(
+            {},
+            [
+                "nsme",
+                "gmail",
+                "role",
+                "about",
+                "img"
+            ],
+            function (err, coaches) {
+                res.setHeader("Content-type", "application/json");
+                res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+                for (var x in coaches) {
+                    coaches[x].about = coaches[x].about.replace(/(?:\r\n|\r|\n)/g, '');
+                }
+                res.send(
+                    coaches
+                );
+            }
+        );
+
+
+    }
+);
+
+module.exports = router;
