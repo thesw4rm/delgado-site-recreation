08966977e8f1129048bbbff6c015f4756f8717ab
Yadunandan Pillai <ytpillai@gmail.com>
2017-04-30 17:52:53 -0400

    added id to fields retrieved

--- routes/api.js
+++ routes/api.js
@@ -14,14 +14,16 @@ router.get(
                 "role",
                 "about",
                 "img",
-                "id"
             ],
             function (err, coaches) {
                 res.setHeader("Content-type", "application/json");
                 res.setHeader("Access-Control-Allow-Origin", "*");
                 res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+                var count = 0;
                 for (var x in coaches) {
                     coaches[x].about = coaches[x].about.replace(/(?:\r\n|\r|\n)/g, '');
+                    coaches[x].objId = count;
+                    count++;
                 }
                 res.send(
                     coaches
